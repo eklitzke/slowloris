f8bc44cf8203b8bcbb2fc7a11db80b27da74086d
Evan Klitzke <evan@eklitzke.org>
2016-08-17 11:37:19 -0700

    .

--- src/loop.cc
+++ src/loop.cc
@@ -1,50 +1,111 @@
 #include "./loop.h"
 
+#include <event2/buffer.h>
 #include <event2/bufferevent.h>
 #include <event2/event.h>
 #include <event2/listener.h>
 
+#include <cassert>
+#include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <random>
+
+#include <unistd.h>
 
 #define READ_TIMEOUT (BEV_EVENT_TIMEOUT | BEV_EVENT_READING)
 
 namespace {
-struct LoopState {
-  timeval timeout;
+std::random_device rand_device;
+std::default_random_engine rand_engine(rand_device());
+
+void OnRead(bufferevent *bev, void *ctx);
+void OnEvent(bufferevent *bev, short events, void *ctx);
+
+int MAX_SECONDS = 1;
+
+int SCORE = 0;
+
+class Socket {
+ public:
+  Socket() = delete;
+  Socket(const Socket &other) = delete;
+  Socket(event_base *base, evutil_socket_t fd)
+      : buf_(bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE)) {
+    assert(buf_ != nullptr);
+    UpdateTimeout();
+
+    bufferevent_setcb(buf_, OnRead, nullptr, OnEvent, this);
+    bufferevent_enable(buf_, EV_READ | EV_WRITE);
+  }
+  ~Socket() { bufferevent_free(buf_); }
+
+  bool IsReady() const {
+    timeval tv;
+    gettimeofday(&tv, nullptr);
+    if (tv.tv_sec == next_.tv_sec) {
+      return tv.tv_usec >= next_.tv_usec;
+    }
+    return tv.tv_sec > next_.tv_sec;
+  }
+
+  void UpdateTimeout() {
+    gettimeofday(&next_, nullptr);
+
+    std::uniform_int_distribution<int> seconds_dist(0, MAX_SECONDS - 1);
+    std::uniform_int_distribution<int> useconds_dist(0, 999999);
+
+    int sec = seconds_dist(rand_engine);
+    int usec = useconds_dist(rand_engine);
+
+    next_.tv_usec += usec;
+    while (next_.tv_usec >= 1000000) {
+      next_.tv_usec -= 1000000;
+      next_.tv_sec++;
+    }
+    next_.tv_sec += sec;
+
+    evbuffer_add_printf(bufferevent_get_output(buf_), "%d.%06d\n", sec, usec);
+  }
+
+ private:
+  timeval next_;
+  bufferevent *buf_;
 };
 
 void OnRead(bufferevent *bev, void *ctx) {
-  std::cout << "data on fd " << bufferevent_getfd(bev) << std::endl;
+  static char buf[8192];
+  bufferevent_read(bev, buf, sizeof(buf));
+
+  Socket *sock = reinterpret_cast<Socket *>(ctx);
+  if (sock->IsReady()) {
+    sock->UpdateTimeout();
+    SCORE++;
+  } else {
+    SCORE -= 10;
+    delete sock;
+  }
+  std::cout << "score = " << SCORE << std::endl;
 }
 
 void OnEvent(bufferevent *bev, short events, void *ctx) {
-  if ((events & READ_TIMEOUT) == READ_TIMEOUT) {
-    std::cout << "connection timed out for fd " << bufferevent_getfd(bev)
-              << std::endl;
-    bufferevent_free(bev);
+  if (events & (BEV_EVENT_ERROR | BEV_EVENT_EOF)) {
+    Socket *sock = reinterpret_cast<Socket *>(ctx);
+    delete sock;
   }
 }
 
-void OnAccept(evconnlistener *listener, evutil_socket_t sock, sockaddr *addr,
+void OnAccept(evconnlistener *listener, evutil_socket_t fd, sockaddr *addr,
               int len, void *ctx) {
-  std::cout << "new connection, fd = " << sock << std::endl;
+  std::cout << "new connection, fd = " << fd << std::endl;
   event_base *base = evconnlistener_get_base(listener);
-  bufferevent *bev = bufferevent_socket_new(base, sock, BEV_OPT_CLOSE_ON_FREE);
-  bufferevent_setcb(bev, OnRead, nullptr, OnEvent, nullptr);
-
-  LoopState *state = reinterpret_cast<LoopState *>(ctx);
-  bufferevent_set_timeouts(bev, &state->timeout, nullptr);
-
-  bufferevent_enable(bev, EV_READ | EV_WRITE);
+  Socket *sock = new Socket(base, fd);
 }
 }
 
 namespace slowloris {
 int RunLoop(int port, int timeout) {
-  LoopState loop_state;
-  loop_state.timeout.tv_sec = timeout;
-  loop_state.timeout.tv_usec = 0;
+  MAX_SECONDS = timeout;
 
   event_base *base = event_base_new();
   if (base == nullptr) {
@@ -58,7 +119,7 @@ int RunLoop(int port, int timeout) {
   listen_addr.sin_addr.s_addr = INADDR_ANY;
   listen_addr.sin_port = htons(port);
   evconnlistener *listener = evconnlistener_new_bind(
-      base, OnAccept, &loop_state, LEV_OPT_REUSEABLE, SOMAXCONN,
+      base, OnAccept, nullptr, LEV_OPT_REUSEABLE, SOMAXCONN,
       (sockaddr *)&listen_addr, sizeof(listen_addr));
   if (listener == nullptr) {
     std::cerr << "failed to bind/listen on port\n";
