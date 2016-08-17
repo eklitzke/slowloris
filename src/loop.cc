f78137787b8f600726a95dbee2e8072a38c9de8b
Evan Klitzke <evan@eklitzke.org>
2016-08-17 01:39:23 -0700

    no more globals

--- src/loop.cc
+++ src/loop.cc
@@ -10,13 +10,11 @@
 #define READ_TIMEOUT (BEV_EVENT_TIMEOUT | BEV_EVENT_READING)
 
 namespace {
-event_base *base = nullptr;
+struct LoopState {
+  timeval timeout;
+};
 
-int SOCKET_TIMEOUT = 1;
-
-void OnRead(bufferevent *bev, void *ctx) {
-  std::cout << "got data on connection" << std::endl;
-}
+void OnRead(bufferevent *bev, void *ctx) { std::cout << "data!" << std::endl; }
 
 void OnEvent(bufferevent *bev, short events, void *ctx) {
   if ((events & READ_TIMEOUT) == READ_TIMEOUT) {
@@ -26,26 +24,26 @@ void OnEvent(bufferevent *bev, short events, void *ctx) {
 }
 
 void OnAccept(evconnlistener *listener, evutil_socket_t sock, sockaddr *addr,
-              int len, void *unused) {
-  std::cout << "got a new connection " << sock << std::endl;
+              int len, void *ctx) {
+  std::cout << "new connection, fd = " << sock << std::endl;
+  event_base *base = evconnlistener_get_base(listener);
   bufferevent *bev = bufferevent_socket_new(base, sock, BEV_OPT_CLOSE_ON_FREE);
   bufferevent_setcb(bev, OnRead, nullptr, OnEvent, nullptr);
 
-  timeval tv;
-  tv.tv_sec = SOCKET_TIMEOUT;
-  tv.tv_usec = 0;
-  bufferevent_set_timeouts(bev, &tv, nullptr);
+  LoopState *state = reinterpret_cast<LoopState *>(ctx);
+  bufferevent_set_timeouts(bev, &state->timeout, nullptr);
 
   bufferevent_enable(bev, EV_READ | EV_WRITE);
 }
 }
 
 namespace slowloris {
-
 int RunLoop(int port, int timeout) {
-  SOCKET_TIMEOUT = timeout;
+  LoopState loop_state;
+  loop_state.timeout.tv_sec = timeout;
+  loop_state.timeout.tv_usec = 0;
 
-  base = event_base_new();
+  event_base *base = event_base_new();
   if (base == nullptr) {
     std::cerr << "failed to event_base_new()\n";
     return 1;
@@ -57,7 +55,7 @@ int RunLoop(int port, int timeout) {
   listen_addr.sin_addr.s_addr = INADDR_ANY;
   listen_addr.sin_port = htons(port);
   evconnlistener *listener = evconnlistener_new_bind(
-      base, OnAccept, nullptr, LEV_OPT_REUSEABLE, SOMAXCONN,
+      base, OnAccept, &loop_state, LEV_OPT_REUSEABLE, SOMAXCONN,
       (sockaddr *)&listen_addr, sizeof(listen_addr));
   if (listener == nullptr) {
     std::cerr << "failed to bind/listen on port\n";
