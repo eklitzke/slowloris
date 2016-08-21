05b9327162ffa096539400ca2e4e0fc275170a1c
Evan Klitzke <evan@eklitzke.org>
2016-08-21 15:06:38 -0700

    remove global variables

--- src/loop.cc
+++ src/loop.cc
@@ -24,26 +24,31 @@ void OnEvent(bufferevent *bev, short events, void *ctx);
 
 std::unordered_set<Socket *> sockets_;
 
-int MAX_SECONDS;
-int MAX_TIMEOUT;
-int SCORE = 0;
+struct GlobalState {
+  int max_seconds;
+  int max_timeout;
+  int score;
+};
 
 class Socket {
  public:
   Socket() = delete;
   Socket(const Socket &other) = delete;
-  Socket(event_base *base, evutil_socket_t fd)
-      : buf_(bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE)) {
+  Socket(event_base *base, evutil_socket_t fd, GlobalState *state)
+      : buf_(bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE)),
+        state_(state) {}
+  ~Socket() {
+    sockets_.erase(this);
+    bufferevent_free(buf_);
+  }
+
+  void Start() {
     assert(buf_ != nullptr);
     UpdateTimeout();
 
     bufferevent_setcb(buf_, OnRead, nullptr, OnEvent, this);
     bufferevent_enable(buf_, EV_READ | EV_WRITE);
     sockets_.insert(this);
-  }
-  ~Socket() {
-    sockets_.erase(this);
-    bufferevent_free(buf_);
   }
 
   bool IsReady() const {
@@ -58,7 +63,7 @@ class Socket {
   void UpdateTimeout() {
     gettimeofday(&next_, nullptr);
 
-    std::uniform_int_distribution<int> seconds_dist(0, MAX_SECONDS - 1);
+    std::uniform_int_distribution<int> seconds_dist(0, state_->max_seconds - 1);
     std::uniform_int_distribution<int> useconds_dist(0, 999999);
 
     int sec = seconds_dist(rand_engine);
@@ -71,13 +76,16 @@ class Socket {
     }
     next_.tv_sec += sec;
 
-    evbuffer_add_printf(bufferevent_get_output(buf_), "%d %d.%06d\n", SCORE,
-                        sec, usec);
+    evbuffer_add_printf(bufferevent_get_output(buf_), "%d %d.%06d\n",
+                        state_->score, sec, usec);
   }
+
+  GlobalState *state() { return state_; }
 
  private:
   timeval next_;
   bufferevent *buf_;
+  GlobalState *state_;
 };
 
 // close all of the sockets
@@ -87,7 +95,6 @@ void CloseAllSockets(event_base *base) {
     delete *it;
   }
   event_base_loopbreak(base);
-  std::cout << SCORE << std::endl;
 }
 
 void Shutdown(evutil_socket_t fd, short what, void *ctx) {
@@ -101,11 +108,15 @@ void OnRead(bufferevent *bev, void *ctx) {
 
   Socket *sock = reinterpret_cast<Socket *>(ctx);
   if (sock->IsReady()) {
-    SCORE++;
+    sock->state()->score++;
     sock->UpdateTimeout();
   } else {
     event_base *base = bufferevent_get_base(bev);
     CloseAllSockets(base);
+
+    // print the score and reset it
+    std::cout << sock->state()->score << std::endl;
+    sock->state()->score = 0;
   }
 }
 
@@ -116,24 +127,25 @@ void OnEvent(bufferevent *bev, short events, void *ctx) {
   }
 }
 
-void OnAccept(evconnlistener *listener, evutil_socket_t fd, sockaddr *addr,
-              int len, void *ctx) {
+void OnAccept(evconnlistener *listener, evutil_socket_t fd,
+              sockaddr *unused_addr, int unused_len, void *ctx) {
+  GlobalState *state = reinterpret_cast<GlobalState *>(ctx);
   event_base *base = evconnlistener_get_base(listener);
   if (sockets_.empty()) {
     event *ev = evtimer_new(base, Shutdown, base);
     timeval tv;
-    tv.tv_sec = MAX_TIMEOUT;
+    tv.tv_sec = state->max_timeout;
     tv.tv_usec = 0;
     evtimer_add(ev, &tv);
   }
-  Socket *sock = new Socket(base, fd);
+  Socket *sock = new Socket(base, fd, state);
+  sock->Start();
 }
 }
 
 namespace slowpoke {
 int RunLoop(int port, int timeout, int max_timeout) {
-  MAX_SECONDS = timeout;
-  MAX_TIMEOUT = max_timeout;
+  GlobalState state{timeout, max_timeout, 0};
 
   event_base *base = event_base_new();
   if (base == nullptr) {
@@ -147,7 +159,7 @@ int RunLoop(int port, int timeout, int max_timeout) {
   listen_addr.sin_addr.s_addr = INADDR_ANY;
   listen_addr.sin_port = htons(port);
   evconnlistener *listener = evconnlistener_new_bind(
-      base, OnAccept, nullptr, LEV_OPT_REUSEABLE, SOMAXCONN,
+      base, OnAccept, &state, LEV_OPT_REUSEABLE, SOMAXCONN,
       (sockaddr *)&listen_addr, sizeof(listen_addr));
   if (listener == nullptr) {
     std::cerr << "failed to bind/listen on port\n";
